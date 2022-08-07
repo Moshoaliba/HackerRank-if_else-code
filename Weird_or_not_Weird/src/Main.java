227410bfb76924133d7ecc27d29ed7fbc574192f
Lehlohonolo <l.wizard.h@gmail.com>
2022-08-07 18:18:13 +0200

    Added comments

--- Weird_or_not_Weird/src/Main.java
+++ Weird_or_not_Weird/src/Main.java
@@ -4,24 +4,25 @@ public class Main {
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
 
+        //Getting user input
         System.out.println("Enter any number to test: ");
         int num = input.nextInt();
 
-        if(num % 2 == 0){
-            if(num > 2 && num < 5)
+        if(num % 2 == 0){   //the number is even
+            if(num > 2 && num < 5)  //the even number is between 2 and 5
             {
                 System.out.println("Even and not weird");
             }
-            else if(num > 6 && num < 20)
+            else if(num > 6 && num < 20)    //the even number is between 6 and 20
             {
                 System.out.println("Even and weird");
             }
-            else
+            else    //default condition
             {
                 System.out.println("Even and not weird");
             }
         }
-        else
+        else    //the number is odd
         {
             System.out.println("Odd and weird");
         }
